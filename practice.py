146347e5c421a3ba782dd741736ea0267bb0a12d
sydteo <71167004+sydteo@users.noreply.github.com>
2022-04-30 16:00:50 +0800

    prac

--- practice.py
+++ practice.py
@@ -1,15 +1,7 @@
-def knapsack(wi, pi, C):
-    profit = []
-    for r in range(C+1):
-        profit.append(0)
-    for r in range(C+1):
-        for c in range(len(wi)):
-            if wi[c] <= r:
-                if profit[r] < profit[r-wi[c]] + pi[c]:
-                    profit[r] = profit[r-wi[c]] + pi[c]
-    print("Running result of {}: ({}}", C, profit)
-    return profit[C]
+def fibo(n):
+    if n==0 or n==1:
+        return n
+    else:
+        return fibo(n-1) + fibo(n-2)
 
-wi = [3,4,5]
-pi = [4,2,9]
-print(knapsack(wi,pi,8))
+print(fibo(6))
